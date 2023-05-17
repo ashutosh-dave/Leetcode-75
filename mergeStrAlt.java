cbd6b35c0355be15be691b7f28578addb83abff5
Ashutosh Dave <50098336+ashutosh-dave@users.noreply.github.com>
2023-05-17 16:08:29 +0530

    Solution of 1768. Merge Strings Alternatively in Java

--- mergeStrAlt.java
+++ mergeStrAlt.java
@@ -1,5 +1,26 @@
-class Solution{
-  public String mergeAlternatively(String word1, String word2){
-    
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder merged = new StringBuilder();
+        int i = 0;
+        int j = 0;
+
+        while(i < word1.length() || j < word2.length()){
+            if (i < word1.length()){
+                merged.append(word1.charAt(i));
+                i++;
+            }
+
+            if(j < word2.length()){
+                merged.append(word2.charAt(j));
+                j++;
+            }
+        }
+        return merged.toString();
+    }
+  public static void main(String args[]){
+    String word1 = "Hello";
+    String word2 = "World";
+    String mergedString = mergeAlternatively(word1, word2);
+    System.out.println(mergedString);
   }
 }
